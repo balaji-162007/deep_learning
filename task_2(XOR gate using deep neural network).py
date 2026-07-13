80fbe01bd7430973914e91ef843d711da01a4b3c
balaji-162007 <balaji46641@gmail.com>
2026-07-13 16:44:19 +0530

    Implement XOR gate using a deep neural network

---
+++ task_2(XOR gate using deep neural network).py
@@ -0,0 +1,33 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+x = np.array([
+    [0,0],
+    [0,1],
+    [1,0],
+    [1,1]
+])
+y = np.array([
+    [0],
+    [1],
+    [1],
+    [0]
+])
+model = Sequential()
+model.add(Dense(units=4, input_dim=2, activation='relu'))
+model.add(Dense(units=1, activation='sigmoid'))
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+model.fit(x, y, epochs=500, verbose=0)
+prediction = model.predict(x)
+print("Input:")
+print(x)
+print("\nExpected Output:")
+print(y)
+print("\nPredicted Output:")
+print(prediction)
+print("\nRounded Prediction:")
+print(np.round(prediction))
