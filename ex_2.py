009a9ea64c5ac927f6589d1aa173400985b81067
balaji-162007 <balaji46641@gmail.com>
2026-07-27 15:22:53 +0530

    clean up ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,24 +1,15 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
 import warnings
 warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
-
 print("Name: Balaji.S")
 print("Reg.No: 814724243022")
-
-# Load MNIST dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-
-# Normalize and reshape
 x_train = x_train.reshape(60000, 28, 28, 1).astype("float32") / 255.0
 x_test = x_test.reshape(10000, 28, 28, 1).astype("float32") / 255.0
-
-# Create CNN model
 model = Sequential([
     Input(shape=(28, 28, 1)),
     Conv2D(32, (3, 3), activation="relu"),
@@ -29,24 +20,15 @@ model = Sequential([
     Dense(128, activation="relu"),
     Dense(10, activation="softmax")
 ])
-
-# Compile model
 model.compile(
     optimizer="adam",
     loss="sparse_categorical_crossentropy",
     metrics=["accuracy"]
 )
-
-# Train model
 model.fit(x_train, y_train, epochs=5, batch_size=64)
-
-# Evaluate model
 loss, accuracy = model.evaluate(x_test, y_test)
-
 print("Test Accuracy:", accuracy)
-
-# Predict first test image
 prediction = model.predict(x_test[0:1])
-
 print("Predicted Character:", prediction.argmax())
 print("Actual Character:", y_test[0])
+
